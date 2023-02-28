d9275fff35fdd6a9ea13cea3bc96be77a5b9579c
kzlynn18 <125532758+kzlynn18@users.noreply.github.com>
2023-02-28 12:35:00 +0800

    Create task1.py

---
+++ specimen1/task1.py
@@ -0,0 +1,27 @@
+#input
+greeting=input()
+name=input()
+print(greeting + ''' ''' + name)
+
+i = int(input())
+a = int(input()) 
+
+if a > i:
+    print(a - i)
+else:
+    print(i - a)
+  
+  word=input()
+result=word
+
+while len(result) <= 30:
+    result += word
+print(result)
+    
+    i=int(input())
+result = i
+if result > 50:
+ print("yes dream big")
+else:
+ print("on the small side")
+
