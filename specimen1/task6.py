f0045b00e8c1b917ff583b85fa0671f7cfb5d85d
kzlynn18 <125532758+kzlynn18@users.noreply.github.com>
2023-03-06 11:50:03 +0800

    Create task6.py

---
+++ specimen1/task6.py
@@ -0,0 +1,15 @@
+Name1 = input()
+Name2 = input()
+Name3 = input()
+Name4 = input()
+Name5 = input()
+if Name1 == "Ellie":
+    print("1st")
+elif Name2 == "Ellie":
+    print("2nd")
+elif Name3 == "Ellie":
+    print("3rd")
+elif Name4 == "Ellie":
+    print("4th")
+elif Name5 == "Ellie":
+    print("5th")
